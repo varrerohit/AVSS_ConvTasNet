0a2af6e3e5ecdbaf125be6e5ad04ebba03baef1c
varrerohit <rohit@blueskeye.com>
2026-05-13 11:21:27 +0530

    chore: isolate demo videos to local directories

--- bskai_demo/run_demo_convtasnet.sh
+++ bskai_demo/run_demo_convtasnet.sh
@@ -2,7 +2,7 @@
 
 # Example execution script
 /Users/rohit/miniconda3/envs/avss/bin/python demo_convtasnet.py \
-    --input ../../dolphin/bskai_demo/demo_videos/mix_main.mp4 \
+    --input ./demo_videos/mix_main.mp4 \
     --output ./demo_videos/mix_main_output \
     --sdk-path /Users/rohit/Desktop/SDKs/BSocial/BM.BSocial-3.1.0-2025-11-11-16-47-build-8f1f318-darwin-arm64 \
     --license-path /Users/rohit/Desktop/SDKs/licence_offline_rnd.bskai \
--- bskai_demo_realtime/run_demo_realtime.sh
+++ bskai_demo_realtime/run_demo_realtime.sh
@@ -2,7 +2,7 @@
 
 # Example execution script for real-time ConvTasNet
 /Users/rohit/miniconda3/envs/avss/bin/python demo_realtime_convtasnet.py \
-    --input ../../dolphin/bskai_demo_realtime/input_videos/mix_main.mp4 \
+    --input ./input_videos/mix_main.mp4 \
     --output ./outputs/mix_main_convtasnet \
     --sdk-path /Users/rohit/Desktop/SDKs/BSocial/BM.BSocial-3.1.0-2025-11-11-16-47-build-8f1f318-darwin-arm64 \
     --license-path /Users/rohit/Desktop/SDKs/licence_offline_rnd.bskai \
